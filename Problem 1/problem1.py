cd0253e9224b91121ee9df27b40b70868412f0cf
Ayo Osonowo <46603881+ayooo1@users.noreply.github.com>
2023-11-30 21:38:51 -0800

    problem 1 done

---
+++ Problem 1/problem1.py
@@ -0,0 +1,23 @@
+file = open('Problem 1\input.txt','r')
+ans = 0
+
+def checknum(s):
+    n = len(s)
+    x = ''
+    for i in range(n):
+        if s[i].isdigit():
+            x += s[i]
+            break
+    
+    for i in range(n-1,-1,-1):
+        if s[i].isdigit():
+            x += s[i]
+            break
+    
+    return int(x)
+
+
+for s in file.readlines():
+    ans += checknum(s.strip())
+
+print(ans)
