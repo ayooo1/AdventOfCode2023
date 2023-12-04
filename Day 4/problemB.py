d82a6e41546fca994d3beeda950c0418b156136e
Ayo Osonowo <46603881+ayooo1@users.noreply.github.com>
2023-12-04 10:54:03 -0800

    Cleaning day 4

--- Day 4/problemB.py
+++ Day 4/problemB.py
@@ -7,18 +7,8 @@ c = Counter()
 for idx,line in enumerate(file.readlines()):
     c[idx+1] += 1
     _,numbers = line.split(':')[0],line.split(':')[1].rstrip().strip()
-    winning,yours = numbers.split('|')[0].strip().split(' '), numbers.split('|')[1].strip().split(' ')
-    win = set()
-    y = set()
-    for x in winning:
-        if x != '':
-            win.add(x)
-    
-    for x in yours:
-        if x != '':
-            y.add(x)
-    
-    w = len(win & y)
+    winning,yours = set(numbers.split('|')[0].strip().split()), set(numbers.split('|')[1].strip().split())
+    w = len(winning & yours)
 
     for i in range(1,w+1):
         c[idx+i+1] += c[idx+1]
