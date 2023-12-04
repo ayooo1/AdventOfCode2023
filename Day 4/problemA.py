d370980b275a4b204145da78fa9abcebbfac9b3b
Ayo Osonowo <46603881+ayooo1@users.noreply.github.com>
2023-12-04 09:11:26 -0800

    Cleaning day 4

--- Day 4/problemA.py
+++ Day 4/problemA.py
@@ -4,7 +4,6 @@ ans = 0
 for line in file.readlines():
     _,numbers = line.split(':')[0],line.split(':')[1].rstrip().strip()
     winning,yours = set(numbers.split('|')[0].strip().split()), set(numbers.split('|')[1].strip().split())
-
     w = len(winning & yours)
     c = 0
     if w:
