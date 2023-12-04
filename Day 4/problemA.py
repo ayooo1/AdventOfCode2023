9ab389dfc1a1d8fa56ad7d3dcfc56100f7c44cdd
Ayo Osonowo <46603881+ayooo1@users.noreply.github.com>
2023-12-04 09:11:05 -0800

    Cleaning day 4

--- Day 4/problemA.py
+++ Day 4/problemA.py
@@ -3,18 +3,9 @@ file = open('Day 4\input.txt','r')
 ans = 0
 for line in file.readlines():
     _,numbers = line.split(':')[0],line.split(':')[1].rstrip().strip()
-    winning,yours = numbers.split('|')[0].strip().split(' '), numbers.split('|')[1].strip().split(' ')
-    win = set()
-    y = set()
-    for x in winning:
-        if x != '':
-            win.add(x)
-    
-    for x in yours:
-        if x != '':
-            y.add(x)
+    winning,yours = set(numbers.split('|')[0].strip().split()), set(numbers.split('|')[1].strip().split())
 
-    w = len(win & y)
+    w = len(winning & yours)
     c = 0
     if w:
         c = 2**(w-1)
