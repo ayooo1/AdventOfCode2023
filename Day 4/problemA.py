4624ed1f008a457272b43a8f8529f201ad7d1a82
Ayo Osonowo <46603881+ayooo1@users.noreply.github.com>
2023-12-03 21:55:33 -0800

    Day 4 Complete

--- Day 4/problemA.py
+++ Day 4/problemA.py
@@ -1,1 +1,27 @@
 file = open('Day 4\input.txt','r')
+
+ans = 0
+for line in file.readlines():
+    _,numbers = line.split(':')[0],line.split(':')[1].rstrip().strip()
+    winning,yours = numbers.split('|')[0].strip().split(' '), numbers.split('|')[1].strip().split(' ')
+    win = set()
+    y = set()
+    for x in winning:
+        if x != '':
+            win.add(x)
+    
+    for x in yours:
+        if x != '':
+            y.add(x)
+
+    w = len(set(win) - set(y))
+    c = 0
+    if w != len(win):
+        c = 1
+        while len(win)-w>1:
+            c*=2
+            w+=1
+    
+    ans += c
+
+print(ans)
--- Day 4/problemB.py
+++ Day 4/problemB.py
@@ -1,1 +1,29 @@
+from collections import Counter
+
 file = open('Day 4\input.txt','r')
+
+ans = 0
+c = Counter()
+for idx,line in enumerate(file.readlines()):
+    c[idx+1] += 1
+    _,numbers = line.split(':')[0],line.split(':')[1].rstrip().strip()
+    winning,yours = numbers.split('|')[0].strip().split(' '), numbers.split('|')[1].strip().split(' ')
+    win = set()
+    y = set()
+    for x in winning:
+        if x != '':
+            win.add(x)
+    
+    for x in yours:
+        if x != '':
+            y.add(x)
+    
+    w = len(win) - len(win - y)
+
+    for i in range(1,w+1):
+        c[idx+i+1] += c[idx+1]
+
+for k in c.values():
+    ans += k
+
+print(ans)
