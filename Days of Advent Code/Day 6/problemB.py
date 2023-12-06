810196df6cf8615b0df749d23671a6b07d2d230c
Ayo Osonowo <46603881+ayooo1@users.noreply.github.com>
2023-12-06 11:33:55 -0800

    Day 6 simple

--- Days of Advent Code/Day 6/problemB.py
+++ Days of Advent Code/Day 6/problemB.py
@@ -4,8 +4,10 @@ file = open('Days of Advent Code\Day 6\input.txt','r')
 t = int("".join(file.readline().split(":")[1].strip().split()))
 d = int("".join(file.readline().split(":")[1].strip().split()))
 
-a, b, c = -1, t, -d
-def quadratic(a, b, c):
-    return [(-b + sqrt((b*b) - 4*a*c)) / (2*a), (-b - sqrt((b*b) - 4*a*c)) / (2*a)]
-l, r = quadratic(a, b, c)
-print(floor(r) - ceil(l) + 1)
+
+count = 0
+for i in range(t+1):
+    if (i*(t-i))>d:
+        count += 1
+
+print(count)
