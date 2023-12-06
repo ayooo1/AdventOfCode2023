627bfe11a901d095828b3a01aef0e2233d43293e
Ayo Osonowo <46603881+ayooo1@users.noreply.github.com>
2023-12-06 00:46:30 -0800

    Day 6 done

--- Days of Advent Code/Day 6/problemA.py
+++ Days of Advent Code/Day 6/problemA.py
@@ -1,1 +1,33 @@
-file = open('Day 6\input.txt','r')
+import numpy
+
+file = open('Days of Advent Code\Day 6\input.txt','r')
+time = [int(x) for x in file.readline().split(':')[1].strip().split()]
+dist = [int(x) for x in file.readline().split(':')[1].strip().split()]
+
+
+def dp(x,t):
+    if t == 0:
+        return x
+
+    if (x,t) in cache:
+        return cache[(x,t)]
+
+    ans = max(dp(x+1,t-1),x*t)
+    lst.append(x*t)
+
+    cache[(x,t)] = ans
+
+    return ans
+
+ret = []
+for i,t in enumerate(time):
+    lst = []
+    cache = {}
+    dp(0,t)
+    c = 0
+    for v in lst:
+        if v > dist[i]:
+            c += 1
+    ret.append(c)
+
+print(numpy.prod(ret))
--- Days of Advent Code/Day 6/problemB.py
+++ Days of Advent Code/Day 6/problemB.py
@@ -1,1 +1,11 @@
-file = open('Day 6\input.txt','r')
+from math import sqrt, floor, ceil
+
+file = open('Days of Advent Code\Day 6\input.txt','r')
+t = int("".join(file.readline().split(":")[1].strip().split()))
+d = int("".join(file.readline().split(":")[1].strip().split()))
+
+a, b, c = -1, t, -d
+def quadratic(a, b, c):
+    return [(-b + sqrt((b*b) - 4*a*c)) / (2*a), (-b - sqrt((b*b) - 4*a*c)) / (2*a)]
+l, r = quadratic(a, b, c)
+print(floor(r) - ceil(l) + 1)
