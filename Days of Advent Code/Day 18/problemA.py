0fad08ddf187d5ae56df1449f236f1f46f957d8a
Ayo Osonowo <46603881+ayooo1@users.noreply.github.com>
2023-12-18 01:51:08 -0600

    day 18 Make Me Math Again

--- Days of Advent Code/Day 18/problemA.py
+++ Days of Advent Code/Day 18/problemA.py
@@ -1,10 +1,38 @@
+from collections import deque
+
 with open('Days of Advent Code\Day 18\input.txt','r') as file:
     fp = [x.strip() for x in file.readlines()]
-    temp = fp
-    fp = []
+    curr = (0,0)
+    moves = {'R': (0,1), 'D': (1,0), 'L': (0,-1), 'U': (-1,0)}
+    pts = []
+    perimeter = 0
+    for line in fp:
+        prev = curr
+        x,y,z = line.strip().split(' ')
+        y = int(y)
+        curr = curr[0]+moves[x][0]*y, curr[1]+moves[x][1]*y
+        pts.append((prev, curr))
+        perimeter += y
+
+    pts.append((curr,(0,0)))
+
+    #shoelace theorem
+    area = 0
+    for e in pts:
+        (a,b) = e
+        area += a[1] * b[0] - a[0] * b[1]
+    area = int(abs(area) / 2)
     
-    for i in range(len(temp)):
-        lst = []
-        for j in range(len(temp[0])):
-            lst.append(int(temp[i][j]))
-        fp.append(lst)
+    #picks theorem
+    interior = int(area - perimeter / 2 + 1)
+
+    print(perimeter + interior)
+    
+
+
+
+        
+    
+
+
+
--- Days of Advent Code/Day 18/problemB.py
+++ Days of Advent Code/Day 18/problemB.py
@@ -1,10 +1,39 @@
+from collections import deque
+
 with open('Days of Advent Code\Day 18\input.txt','r') as file:
     fp = [x.strip() for x in file.readlines()]
-    temp = fp
-    fp = []
+    curr = (0,0)
+    moves = {0: (0,1), 1: (1,0), 2: (0,-1), 3: (-1,0)}
+    pts = []
+    perimeter = 0
+    for line in fp:
+        prev = curr
+        x,y,z = line.strip().split(' ')
+        z = z.strip()
+        y,x = int(z[2:7],16),int(z[7])
+        curr = curr[0]+moves[x][0]*y, curr[1]+moves[x][1]*y
+        pts.append((prev, curr))
+        perimeter += y
+
+    pts.append((curr,(0,0)))
+
+    #shoelace
+    area = 0
+    for e in pts:
+        (a,b) = e
+        area += a[1] * b[0] - a[0] * b[1]
+    area = int(abs(area) / 2)
+
+    #picks theorem
+    sq = int(area - perimeter / 2 + 1)
+
+    print(perimeter + interior)
     
-    for i in range(len(temp)):
-        lst = []
-        for j in range(len(temp[0])):
-            lst.append(int(temp[i][j]))
-        fp.append(lst)
+
+
+
+        
+    
+
+
+
